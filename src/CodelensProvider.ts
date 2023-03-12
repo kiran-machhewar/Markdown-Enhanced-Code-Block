8251813922e189abab1f693e980f12ee32247473
smachhewar <smachhewar@gmail.com>
2023-03-12 12:30:31 +0530

    Configuration to hide run button Fixes #5

--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import { getScriptToRunCode } from "./MarkdownUtil";
 
 /**
- * Codelense provider which finds the code blocks in the markdown files 
+ * Codelense provider which finds the code blocks in the markdown files
  * and provides commands to operate on those
  */
 export class CodelensProvider implements vscode.CodeLensProvider {
@@ -20,12 +20,12 @@ export class CodelensProvider implements vscode.CodeLensProvider {
     });
   }
 
-	/**
-	 * Provides the code lenses for markdown code blocks	 
-	 * @param document Current opened document
-	 * @param token Cancellation Taken
-	 * @returns vscode.CodeLens[] list of codelenses
-	 */
+  /**
+   * Provides the code lenses for markdown code blocks
+   * @param document Current opened document
+   * @param token Cancellation Taken
+   * @returns vscode.CodeLens[] list of codelenses
+   */
   public provideCodeLenses(
     document: vscode.TextDocument,
     token: vscode.CancellationToken
@@ -40,9 +40,9 @@ export class CodelensProvider implements vscode.CodeLensProvider {
       while ((matches = regex.exec(text)) !== null) {
         const reg = new RegExp(/(.+)/g);
         let content: string = matches[0];
-        let codeLines = content.split('\n');
-        codeLines.splice(0,1);        
-        let code = codeLines.join('\n').replace(/`/g, "");
+        let codeLines = content.split("\n");
+        codeLines.splice(0, 1);
+        let code = codeLines.join("\n").replace(/`/g, "");
         // Add Copy Command
         const line = document.lineAt(document.positionAt(matches.index).line);
         const copyPosition = new vscode.Position(line.lineNumber, 0);
@@ -58,34 +58,41 @@ export class CodelensProvider implements vscode.CodeLensProvider {
         if (copyRange) {
           this.codeLenses.push(new vscode.CodeLens(copyRange, copyCommand));
         }
-        
-        // Add Run Code Command
         const runPosition = new vscode.Position(line.lineNumber, 5);
-        let codeConfigs = content.split('\n')[0].replace(/`/g, "");        
-        let details = {
-          'position' : runPosition,
-          'language' : codeConfigs.split('|')[0],
-          'org':''
-        };        
-        if( ( details.language === 'apex' || 
-            details.language === 'soql') && codeConfigs.split('|').length > 0) {
-          details.org = codeConfigs.split('|')[1];          
-        }
-        
-        if(details.language){
-          code = getScriptToRunCode(code,details); 
-        }      
-        const runCommand: vscode.Command = {
-          title: "Run",
-          command: "markdown-copy-code.runcode",
-          arguments: [code, details, false],
-        };
-        const runRange = document.getWordRangeAtPosition(
-          runPosition,
-          new RegExp(reg)
-        );
-        if (runRange) {
-          this.codeLenses.push(new vscode.CodeLens(runRange, runCommand));
+        // Add Run Code Command
+        if (
+          !vscode.workspace
+            .getConfiguration("markdown-enhanced-code-block")
+            .get("disableRunButton", true)
+        ) {
+          let codeConfigs = content.split("\n")[0].replace(/`/g, "");
+          let details = {
+            position: runPosition,
+            language: codeConfigs.split("|")[0],
+            org: "",
+          };
+          if (
+            (details.language === "apex" || details.language === "soql") &&
+            codeConfigs.split("|").length > 0
+          ) {
+            details.org = codeConfigs.split("|")[1];
+          }
+
+          if (details.language) {
+            code = getScriptToRunCode(code, details);
+          }
+          const runCommand: vscode.Command = {
+            title: "Run",
+            command: "markdown-copy-code.runcode",
+            arguments: [code, details, false],
+          };
+          const runRange = document.getWordRangeAtPosition(
+            runPosition,
+            new RegExp(reg)
+          );
+          if (runRange) {
+            this.codeLenses.push(new vscode.CodeLens(runRange, runCommand));
+          }
         }
 
         // Add Replace Variables Command
